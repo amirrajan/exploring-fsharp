9022b132437c69b069c3b3dc14751d56c4dd870d
Amir Rajan <ar@amirrajan.net>
2016-01-29 23:03:03 -0600

    nuget 'integration'

---
+++ build.sh
@@ -0,0 +1,2 @@
+mono nuget.exe  install Http.fs -ExcludeVersion -OutputDirectory FSharpModules -Version 1.5.1
+mono nuget.exe  install Newtonsoft.Json -ExcludeVersion -OutputDirectory FSharpModules -Version 6.0.1
