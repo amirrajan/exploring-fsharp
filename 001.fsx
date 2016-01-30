32925a774b09499721c40544ac88eddd666cd24b
Amir Rajan <ar@amirrajan.net>
2016-01-29 22:40:52 -0600

    simple class with some working stuff

---
+++ 001.fsx
@@ -0,0 +1,16 @@
+open System
+
+printfn "hello world"
+
+let blah = 1 + 1
+
+System.Console.WriteLine("booya")
+
+type Person =
+  { FirstName : string; LastName : string; }
+  member this.FullName = this.FirstName + " " + this.LastName
+  member this.Greet other = other.FirstName + " " + other.LastName + " says hello to " + this.FullName
+
+
+let person = { FirstName = "Amir"; LastName = "Rajan" }
+printfn "%s" (person.Greet person)
