31a605121ae443701691ebec0b3b157b24887aa6
Amir Rajan <ar@amirrajan.net>
2016-04-04 21:55:48 -0500

    moved build to sub folder

---
+++ 002/build.sh
@@ -0,0 +1,3 @@
+mono ../nuget.exe install Http.fs -ExcludeVersion -OutputDirectory FSharpModules -Version 1.5.1
+mono ../nuget.exe install Newtonsoft.Json -ExcludeVersion -OutputDirectory FSharpModules -Version 6.0.1
+mono ../nuget.exe install FSharp.Data -ExcludeVersion -OutputDirectory FSharpModules -Version 2.2.3
